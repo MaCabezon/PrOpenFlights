0b65e92fc2a0301dcf67d990d5ae6aa3309ba244
MaCabezon <miguel.cabezon@alumnos.uneatlantico.es>
2018-11-21 14:12:05 +0100

    commit1

---
+++ PrOpenFlights/src/es/uneatlantico/common/MainScreen.java
@@ -0,0 +1,10 @@
+package es.uneatlantico.common;
+
+public class MainScreen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("FUNCIONA");
+	}
+
+}
